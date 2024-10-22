d78079ba10feb2e5c2a0662e57b439d7e87cbbcf
MD Alimuzzaman Haris <mdalimuzzaman437@gmail.com>
2024-10-22 17:24:05 +0600

    Initial project setup and added header, blog component

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Blogs from "./components/Blogs"
+import Header from "./components/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header></Header>
+      <h1>Knowledge Cafe</h1>
+      <Blogs></Blogs>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+            <h2 className="text-3xl font-bold">Knowledge Cafe</h2>
+        </div>
+    );
+};
+
+export default Header;
